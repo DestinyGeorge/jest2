f24d81e1ae333f2656a25918f47a0fe36ee8b71a
DestinyGeorge <DestinyGeorge2517@gmail.com>
2026-09-21 02:46:18 +0100

    create addTurn function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,5 +1,5 @@
 const { expect, describe, beforeAll } = require("@jest/globals");
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -39,14 +39,15 @@ describe("newGame works correctly", () => {
         newGame();
     });
 
+    
     test("should set game score to zero", () =>{
         expect(game.score).toEqual(0);
     });
+    test("should be one move in the currentGame array", () =>{
+        expect(game.currentGame.length).toBe(1);
+    });
     test("should clear the playerMoves array", () =>{
         expect(game.playerMoves.length).toBe(0);
-    });
-    test("should clear the currentGame array", () =>{
-        expect(game.currentGame.length).toBe(0);
     });
     test("should display 0 for the element with id of score", () =>{
         expect(document.getElementById("score").innerText).toEqual(0);

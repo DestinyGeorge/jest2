183e8da2564210d383f6c97ffe46e1e6ee340f3f
DestinyGeorge <DestinyGeorge2517@gmail.com>
2026-09-21 03:14:24 +0100

    test and create lightsOn function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,5 +1,5 @@
-const { expect, describe, beforeAll } = require("@jest/globals");
-const { game, newGame, showScore, addTurn } = require("../game");
+const { expect, describe, beforeAll, beforeEach, afterEach } = require("@jest/globals");
+const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -52,4 +52,29 @@ describe("newGame works correctly", () => {
     test("should display 0 for the element with id of score", () =>{
         expect(document.getElementById("score").innerText).toEqual(0);
     });
+});
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    })
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
+    });
+
 });
